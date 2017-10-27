6475df190e9048a94b95795b0e88fb2200af7024
Edi Carlos note <edi_csilva@hotmail.com>
2017-10-27 17:49:59 -0200

    servicos

--- js/main.js
+++ js/main.js
@@ -1,34 +1,14 @@
 $(function() {
-    // equivalente ao load da página
-    // alert("olá!");
-
-    $("#modal01").modal({ backdrop: false, keyboard: false });
-
-    $("#modal01").modal("show");
-
-    setTimeout(function() {
-        $("#modal01").modal("hide");
-    }, 3000);
-
-    $(".ttp").tooltip({
-        title: "Título padrão",
-        trigger: "click"
+    $('a[href*=#]:not([href=#])').not(".carousel-control").click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
     });
-
-    $(".ppv").popover({
-        title: "Título do popover",
-        trigger: "hover focus"
-    });
-
-    $(".btnjs").button();
-
-    $("#troca-estado").click(function() {
-        var btn = $(this);
-        btn.button("loading");
-
-        setTimeout(function() {
-            btn.button("reset");
-        }, 3000);
-    });
-
 });
